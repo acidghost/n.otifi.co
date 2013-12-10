56911c5ff684ee277718f879d61b15d3bade4d60
Andrea Jemmett <andreajemmett@gmail.com>
2013-12-10 10:43:02 +0100

    Added CONFIGS constant to rootScope and createa a basic sign-up form

--- assets/linker/scripts/app.js
+++ assets/linker/scripts/app.js
@@ -7,6 +7,10 @@ angular.module('n.coApp', ['n.coResources'])
         templateUrl: 'linker/views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/signup', {
+        templateUrl: 'linker/views/signup.html',
+        controller: 'SignupCtrl'
+      })
       .when('/search/:artist', {
         templateUrl: 'linker/views/search.results.html',
         controller: 'SearchResCtrl',
@@ -21,5 +25,9 @@ angular.module('n.coApp', ['n.coResources'])
       });
   })
   .constant('CONFIGS', {
+    appName: 'n.otifi.co',
     apiPrefix: '/api/v1'
+  })
+  .run(function($rootScope, CONFIGS) {
+    $rootScope.CONFIGS = CONFIGS;
   });
---
+++ assets/linker/scripts/controllers/signup.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('n.coApp')
+  .controller('SignupCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+
+      $scope.signupFormTemplate = 'linker/views/partials/signup.form.html';
+
+  }]);
